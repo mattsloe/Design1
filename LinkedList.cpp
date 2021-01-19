17899edaeffc67196f74f9250246a81b26b4d3d9
Matt L <mattsloe@gmail.com>
2021-01-18 18:15:29 -0800

    Updated LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -14,7 +14,56 @@ template<class type> LinkedList<type>::LinkedList(const LinkedList<type>& list)
 
 }
 
+template<class type> LinkedList<type>::~LinkedList()
+{
+
+}
+
 template<class type> int LinkedList<type>::isEmpty() const
+{
+
+}
+
+template<class type> int LinkedList<type>::isEmpty() const
+{
+
+}
+template<class type> int LinkedList<type>::listLength() const
+{
+
+}
+
+template<class type> int LinkedList<type>::insert(const type& new_entry, int index)
+{
+
+}
+
+template<class type> int LinkedList<type>::remove(int index)
+{
+
+}
+
+template<class type> int LinkedList<type>::clear()
+{
+
+} 
+
+template<class type> type& LinkedList<type>::getEntry(int index) const
+{
+
+}
+
+template<class type> int LinkedList<type>::setEntry(const type& newEntry, int index)
+{
+
+}
+
+template<class type> int addNode(Node<type> to_add)
+{
+
+}
+
+template<class type> Node<type>* LinkedList<type>::getNodeAt(int index) const
 {
 
 }
